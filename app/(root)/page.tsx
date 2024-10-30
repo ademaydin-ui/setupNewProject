35e52e933b32befd601d710bbfc19d43179338ae
Adem <ademaydin@memoglu.org>
2024-10-30 01:00:02 +0100

    Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitchen Sie Ihr Startup, <br /> vernetzen Sie sich mit Unternehmern
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Reichen Sie Ideen ein, stimmen Sie über Pitches ab und machen Sie in
+          virtuellen Wettbewerben auf sich aufmerksam.
+        </p>
+      </section>
     </>
   );
 }
